640941029ba304725ee0f4466486c890d82874a3
Andrew McKinley <drew2ya@gmail.com>
2015-11-22 14:36:02 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ jQuery(document).ready(function ($) {
     $(window).stellar();
     //Cache some variables
     var links = $('.navigation').find('li');
+    mobileLinks = $('.mobileNavbar').find('li');
     slide = $('.slide');
     button = $('.button');
     mywindow = $(window);
@@ -33,14 +34,39 @@ jQuery(document).ready(function ($) {
     //Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
     //easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
     function goToByScroll(dataslide) {
+        var offset = 0;
+        if (dataslide == 3)
+        {
+            offset = -200;
+        }
+        
+        if (dataslide == 10) {
+            offset = -200;
+        }
+        
+        if (dataslide == 14) {
+            offset = 5;
+        }
+        
+        if (dataslide == 15) {
+            offset = -200;
+        }
         htmlbody.animate({
-            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top + offset
         }, 2000, 'easeInOutQuint');
     }
     //When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
     links.click(function (e) {
         e.preventDefault();
         dataslide = $(this).attr('data-slide');
+        
+        goToByScroll(dataslide);
+    });
+    
+    mobileLinks.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        
         goToByScroll(dataslide);
     });
     //When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
--- js/ourjquery.js
+++ js/ourjquery.js
@@ -2,33 +2,85 @@ jQuery(document).ready(function ($) {
      (function() {
      
      var documentEl = $(document),
-         textCenter = $('#text-center')
-         secondPage = $('#slide2')
+         textCenter = $('#text-center'),
+         secondPage = $('#container2'),
+         thirdPage = $('#container3'),
+         fourthPage = $('#container4'),
+         partyPage = $('#container9'),
+         registryPage = $('#container10'),
+         galleryPage = $('#container14'),
+         rsvpPage = $('#container15'),
+         weddingTitle = $('#theWedding'),
          navbackground = $(".white-shadow");
-         ;
-     
-     documentEl.on('scroll', function() {
-         var currScrollPos = documentEl.scrollTop();
-         var secondPagePos = documentEl.offset();
-         var height = $(window).scrollTop()+55;
+
          
-         if (height >= secondPage.offset().top)
-         {
-             textCenter.text("Love Story");
-             navbackground.css("opacity", 1);
-         }
-         else
+     documentEl.on('scroll', function() {
+         var height = $(window).scrollTop();
+    
+         if (height < secondPage.offset().top)
          {
              textCenter.text("May 6, 2016");
              navbackground.css("opacity", 0.8);
          }
+        else {
+            
+         navbackground.css("opacity", 1);
+        }
+        
+        var titleHeight = $(window).scrollTop() +160;
+        var delayHeight = $(window).scrollTop()+300;
+         if (titleHeight < secondPage.offset().top)
+         {
+             textCenter.text("May 6, 2016");
+             textCenter.fadeIn(700);
+         }
+         else if (titleHeight >= secondPage.offset().top && delayHeight < thirdPage.offset().top)
+         {
+             textCenter.text("Love Story");
+             textCenter.fadeIn(700);
+             
+         }
+         else if (height+400 >= thirdPage.offset().top && height < fourthPage.offset().top)
+         {
+            textCenter.text("The Wedding"); 
+         }
+         else if (height+180 >= thirdPage.offset().top && height-50 < fourthPage.offset().top)
+         {
+            textCenter.fadeOut(700);
+         }
+
+         else if ((height >= partyPage.offset().top-350) && height < partyPage.offset().top-150)
+         {
+            textCenter.text("The Party");
+            textCenter.fadeIn(700);
+         }
+         else if ((height >= registryPage.offset().top-450) && height < registryPage.offset().top-200)
+         {
+            textCenter.text("The Registry");
+            textCenter.fadeIn(700);
+         }
+         else if (height >= galleryPage.offset().top-200 && height < galleryPage.offset().top+5)
+         {
+            textCenter.text("Our Gallery");
+            textCenter.fadeIn(700);
+         }
+         else if (height >= rsvpPage.offset().top-320 && height < rsvpPage.offset().top-130)
+         {
+            textCenter.text("RSVP");
+            textCenter.fadeIn(700);
+         }
+         else
+         {
+            textCenter.fadeOut(700);
+         }
+         
      });
      
-     $( "#drewImageContainer" ).hover(
+     $( "#leftHalf" ).hover(
         function()
         {
             $("#drewText").css("opacity", 1);
-            $("#drewImage").css("opacity", 0.3);
+            $("#drewImage").css("opacity", 0.05);
             $("#gelText").css("opacity", 0);
             $("#gelImage").css("opacity", 1);
         },
@@ -41,11 +93,11 @@ jQuery(document).ready(function ($) {
         }
      );
      
-     $( "#gelImageContainer" ).hover(
+     $( "#rightHalf" ).hover(
         function()
         {
             $("#gelText").css("opacity", 1);
-            $("#gelImage").css("opacity", 0.2);
+            $("#gelImage").css("opacity", 0.05);
             $("#drewText").css("opacity", 0);
             $("#drewImage").css("opacity", 1);
         },
@@ -57,8 +109,150 @@ jQuery(document).ready(function ($) {
             $("#drewImage").css("opacity", 1);
         }
      );
- 
- 
      
+      
+    $("#monogram").click(function (e) {
+       htmlbody.animate({
+            scrollTop: $("#slide1").offset().top
+        }, 2000, 'easeInOutQuint');
+    });
+
+   // $("#container9").masonry();
+     
+     
+     $('#partyPicText1').hover(
+        function()
+        {
+            $('#partyPic1').css("opacity", 0.1);
+            $('#partyText1').css("opacity", 1);
+        },
+        function()
+        {
+            $('#partyPic1').css("opacity", 1);
+            $('#partyText1').css("opacity", 0);
+        }
+     );
+     
+     $('#partyPicText2').hover(
+        function()
+        {
+            $('#partyPic2').css("opacity", 0.1);
+            $('#partyText2').css("opacity", 1);
+        },
+        function()
+        {
+            $('#partyPic2').css("opacity", 1);
+            $('#partyText2').css("opacity",0);
+        }
+     );
+     
+     $('#partyPicText3').hover(
+        function()
+        {
+            $('#partyPic3').css("opacity", 0.1);
+            $('#partyText3').css("opacity", 1);
+        },
+        function()
+        {
+            $('#partyPic3').css("opacity", 1);
+            $('#partyText3').css("opacity", 0);
+        }
+     );
+     
+     $('#partyPicText4').hover(
+        function()
+        {
+            $('#partyPic4').css("opacity", 0.1);
+            $('#partyText4').css("opacity", 1);
+        },
+        function()
+        {
+            $('#partyPic4').css("opacity", 1);
+            $('#partyText4').css("opacity", 0);
+        }
+     );
+     
+     $('#partyPicText5').hover(
+        function()
+        {
+            $('#partyPic5').css("opacity", 0.1);
+            $('#partyText5').css("opacity", 1);
+        },
+        function()
+        {
+            $('#partyPic5').css("opacity", 1);
+            $('#partyText5').css("opacity", 0);
+        }
+     );
+     
+     $('#partyPicText6').hover(
+        function()
+        {
+            $('#partyPic6').css("opacity", 0.1);
+            $('#partyText6').css("opacity", 1);
+        },
+        function()
+        {
+            $('#partyPic6').css("opacity", 1);
+            $('#partyText6').css("opacity", 0);
+        }
+     );
+     
+     $('#partyPicText7').hover(
+        function()
+        {
+            $('#partyPic7').css("opacity", 0.1);
+            $('#partyText7').css("opacity", 1);
+        },
+        function()
+        {
+            $('#partyPic7').css("opacity", 1);
+            $('#partyText7').css("opacity", 0);
+        }
+     );
+     
+     $('#partyPicText8').hover(
+        function()
+        {
+            $('#partyPic8').css("opacity", 0.1);
+            $('#partyText8').css("opacity", 1);
+        },
+        function()
+        {
+            $('#partyPic8').css("opacity", 1);
+            $('#partyText8').css("opacity", 0);
+        }
+     );
+     
+     $('#partyPicText9').hover(
+        function()
+        {
+            $('#partyPic9').css("opacity", 0.1);
+            $('#partyText9').css("opacity", 1);
+        },
+        function()
+        {
+            $('#partyPic9').css("opacity", 1);
+            $('#partyText9').css("opacity", 0);
+        }
+     );
+     
+     $('#partyPicText10').hover(
+        function()
+        {
+            $('#partyPic10').css("opacity", 0.1);
+            $('#partyText10').css("opacity", 1);
+        },
+        function()
+        {
+            $('#partyPic10').css("opacity", 1);
+            $('#partyText10').css("opacity", 0);
+        }
+     );
+     
+    $("#myCarousel").click(function(){
+        textCenter.fadeOut(700);
+    });
+
  })();
 });
